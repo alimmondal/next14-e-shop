fa2ee244b6b6e8590bb1130c1e95cc0884b621e1
alimmondal <italimbd@gmail.com>
2024-01-09 07:04:19 +0600

    upd

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,21 @@
-const Admin = () => {
-  return <div>Admin</div>;
+import getOrders from "@/actions/getOrders";
+import getProducts from "@/actions/getProducts";
+import getUsers from "@/actions/getUsers";
+import Container from "../components/Container";
+import Summary from "./Summary";
+
+const Admin = async () => {
+  const products = await getProducts({ category: null });
+  const orders = await getOrders();
+  const users = await getUsers();
+
+  return (
+    <div className="pt-8">
+      <Container>
+        <Summary products={products} orders={orders} users={users} />
+      </Container>
+    </div>
+  );
 };
 
 export default Admin;
