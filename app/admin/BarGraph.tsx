e939b32040a363e66bca1493db8d65112ef9be90
alimmondal <italimbd@gmail.com>
2024-01-09 16:44:48 +0600

    upd

--- app/admin/BarGraph.tsx
+++ app/admin/BarGraph.tsx
@@ -1,7 +1,56 @@
+"use client";
 import React from "react";
 
-const BarGraph = () => {
-  return <div></div>;
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Tooltip,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
+interface BarGraphProps {
+  data: GraphData[];
+}
+
+type GraphData = {
+  day: string;
+  date: string;
+  totalAmount: number;
+};
+
+const BarGraph: React.FC<BarGraphProps> = ({ data }) => {
+  //   console.log("graph", data);
+  const labels = data.map((item) => item.day);
+  const amounts = data.map((item) => item.totalAmount);
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Sale Amount",
+        data: amounts,
+        backgroundColor: "rgba(75,192,192,0.6)",
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options}></Bar>;
 };
 
 export default BarGraph;
